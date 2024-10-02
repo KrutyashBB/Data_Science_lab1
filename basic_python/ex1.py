9e88df425cf3115b08971654f96b9f2e3ebaa6d2
Krutyash <kostrov2525@gmail.com>
2024-10-02 23:34:51 +0500

    Основы_python - Задача 1 - Исправил ошибку в тестах

--- basic_python/ex1.py
+++ basic_python/ex1.py
@@ -8,10 +8,10 @@ def count_vowel_letters(string: str):
 
 
 assert count_vowel_letters("abbc") == 1, f"Ожидаемый ответ: 1, Текущий ответ: {count_vowel_letters('abbc')}"
-assert count_vowel_letters("") == 0, f"Ожидаемый ответ: 0, Текущий ответ: {count_vowel_letters('abbc')}"
+assert count_vowel_letters("") == 0, f"Ожидаемый ответ: 0, Текущий ответ: {count_vowel_letters('')}"
 assert count_vowel_letters("bbc") == 0, f"Ожидаемый ответ: 0, Текущий ответ: {count_vowel_letters('bbc')}"
 assert count_vowel_letters("aaa") == 3, f"Ожидаемый ответ: 3, Текущий ответ: {count_vowel_letters('aaa')}"
 assert count_vowel_letters("aeiiou") == 6, f"Ожидаемый ответ: 6, Текущий ответ: {count_vowel_letters('aeiiou')}"
-assert count_vowel_letters("AaEEiUu") == 7, f"Ожидаемый ответ: 7, Текущий ответ: {count_vowel_letters('aeiiou')}"
+assert count_vowel_letters("AaEEiUu") == 7, f"Ожидаемый ответ: 7, Текущий ответ: {count_vowel_letters('AaEEiUu')}"
 
 print("Все тесты прошли успешно!")
